d677f3b8cccc86d7aad892ade30704d67b185a5d
bobbert <91771103+thelordbobbert@users.noreply.github.com>
2025-01-14 11:44:18 -0800

    fixed display

--- index.js
+++ index.js
@@ -4,11 +4,19 @@ let bclick = 1;
 let clickMultiplier = 1;
 let totalCookies = 0;
 
+//click handler for the biscuit clicker button
 function clickHandler() {
+    //updates their total cookies variable
     totalCookies +=  bclick * clickMultiplier;
+    //displays this variable by updating display
     updateTotalCookies();
 }
 
+//update cookies
 function updateTotalCookies() {
+    //sets the total cookies in html to the totalCookies variable using the element id
     document.getElementById("totalCookies").innerHTML = totalCookies;
 }
+
+//display on page load
+updateTotalCookies();
