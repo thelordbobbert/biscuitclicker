db9d1d9eb9d831e9b12be20c830512a5db4e6942
bobbert <91771103+thelordbobbert@users.noreply.github.com>
2025-01-14 12:07:54 -0800

    changed total cookies to total biscuits

--- index.js
+++ index.js
@@ -1,22 +1,27 @@
-let multiplier = 1;
-let bpm = 0;
+//auto biscuits per second multi
+let bpsMultiplier = 1;
+//biscuits per second (before multi)
+let bps = 0;
+//biscuits per click (before multi)
 let bclick = 1;
+//biscuits clicked multi
 let clickMultiplier = 1;
-let totalCookies = 0;
+//current total amount of biscuits
+let totalBiscuits = 0;
 
 //click handler for the biscuit clicker button
 function clickHandler() {
-    //updates their total cookies variable
-    totalCookies +=  bclick * clickMultiplier;
+    //updates their total biscuits variable
+    totalBiscuits +=  bclick * clickMultiplier;
     //displays this variable by updating display
-    updateTotalCookies();
+    updateTotalBiscuits();
 }
 
-//update cookies
-function updateTotalCookies() {
-    //sets the total cookies in html to the totalCookies variable using the element id
-    document.getElementById("totalCookies").innerHTML = totalCookies;
+//update biscuits
+function updateTotalBiscuits() {
+    //sets the total biscuits in html to the totalBiscuits variable using the element id
+    document.getElementById("totalBiscuits").innerHTML = totalBiscuits;
 }
 
 //display on page load
-updateTotalCookies();
+updateTotalBiscuits();
