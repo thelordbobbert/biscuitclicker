f8d063e701e05b652e063115a1b4c77469332f85
bobbert <91771103+thelordbobbert@users.noreply.github.com>
2025-01-14 13:29:41 -0800

    added slavery

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ let bclick = 1;
 let clickMultiplier = 1;
 //current total amount of biscuits
 let totalBiscuits = 0;
+//current total amount of slaves
+let slaveAmount = 0;
 
 //click handler for the biscuit clicker button
 function clickHandler() {
@@ -17,11 +19,34 @@ function clickHandler() {
     updateTotalBiscuits();
 }
 
+//allows the user to buy slaves
+function buySlave() {
+    if (totalBiscuits >= 100) {
+        totalBiscuits -= 100;
+        slaveAmount += 1;
+    }
+    updateSlaveOwnership();
+}
+
 //update biscuits
 function updateTotalBiscuits() {
     //sets the total biscuits in html to the totalBiscuits variable using the element id
     document.getElementById("totalBiscuits").innerHTML = totalBiscuits;
 }
 
+//update bps
+function updateBps() {
+    //sets the total cps in html to the bps variable using the element id
+    document.getElementById("bps").innerHTML = bps;
+}
+
+//update slaves
+function updateSlaveOwnership() {
+    //sets the total slaves in html to the slaveAmount variable using the element id
+    document.getElementById("slaves").innerHTML = slaveAmount;
+}
+
 //display on page load
 updateTotalBiscuits();
+updateBps();
+updateSlaveOwnership();
