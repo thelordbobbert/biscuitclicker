22bc0f59cde6ec0308e0fb939a650a7b78ee67dd
kzwln <kenpo.kailoren@gmail.com>
2025-01-14 10:10:38 -0800

    added new variables to store multipliers and click per second, made alert to print the totalCookies.

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 let multiplier = 0;
 let bpm = 0;
+let bclick = 0;
+let clickMultiplier = 0;
+let totalCookies = 0;
 
 function clickHandler() {
-    alert("something");
+    totalCookies +=  bclick * clickMultiplier;
+    alert(totalCookies);
 }
